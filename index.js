12fa7009d61b51d275b27d9f90d3073fd89fc08d
Erik Huang <ekhu94@gmail.com>
2021-03-22 06:24:40 -0700

    make public folder with static files

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+
 //! required one time each project
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+//!
 
 app.get('/', (req, res) => {
     res.render('home');
