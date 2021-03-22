7d542dde2244c431f42961fe98c7f72f4c70faac
Erik Huang <ekhu94@gmail.com>
2021-03-21 21:06:07 -0700

    add basic ejs tag

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.send('Template time!');
+    res.render('home');
 });
 
 app.listen(port, () => {
