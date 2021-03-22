cbb84a17ebad462ea3294d4cdcc39af00389091d
Erik Huang <ekhu94@gmail.com>
2021-03-22 05:36:35 -0700

    passes in variable in object on render

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+//! required one time each project
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.get('/random', (req, res) => {
+    const num = Math.floor(Math.random() * 100) + 1;
+    res.render('random', { num })
 });
 
 app.listen(port, () => {
